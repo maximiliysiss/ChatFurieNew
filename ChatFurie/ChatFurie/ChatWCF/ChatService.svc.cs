0fc715aabe83e686bda703227cee5915bf77ba71
Maxim Zimin <zim.ma2010@yandex.ru>
2018-09-21 11:12:24 +0300

    Почистил каталог

--- ChatFurie/ChatFurie/ChatWCF/ChatService.svc.cs
+++ ChatFurie/ChatFurie/ChatWCF/ChatService.svc.cs
@@ -7,8 +7,6 @@ using System.Text;
 
 namespace ChatWCF
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select ChatService.svc or ChatService.svc.cs at the Solution Explorer and start debugging.
     public class ChatService : IChat
     {
         public bool AddFriend(int user, int friend)
