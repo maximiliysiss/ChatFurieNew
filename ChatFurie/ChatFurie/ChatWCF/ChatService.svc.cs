2419e7e7f2c95e728d204f3bb684608b61101a91
Maxim Zimin <zim.ma2010@yandex.ru>
2018-10-18 12:59:33 +0300

    Add models

--- ChatFurie/ChatFurie/ChatWCF/ChatService.svc.cs
+++ ChatFurie/ChatFurie/ChatWCF/ChatService.svc.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using ChatFurie.Models;
 using ChatWCF.Models.SendModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChatWCF
 {
@@ -12,17 +14,34 @@ namespace ChatWCF
     {
         public bool AddFriend(int user, int friend)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public List<ConversationUserSM> ConversationUserList(int ID)
         {
-            throw new NotImplementedException();
+            ChatContextWCF chatContextWCF = new ChatContextWCF();
+            //var list = chatContextWCF.UserInConversation.Select(x => new ConversationUserSM(x));
+            return new List<ConversationUserSM>();
         }
 
         public bool DeleteFriend(int user, int friend)
         {
-            throw new NotImplementedException();
+            ChatContextWCF chatContextWCF = new ChatContextWCF();
+            var dFriend = chatContextWCF.Users.Find(friend);
+            if (dFriend != null)
+                chatContextWCF.Entry(dFriend).State = EntityState.Deleted;
+            else
+                return false;
+            return true;
+        }
+
+        public List<UserSM> FindFriends(string keys, int user)
+        {
+            ChatContextWCF chatContextWCF = new ChatContextWCF();
+            var result = chatContextWCF.Users.Where(x => x.Name.Contains(keys) && x.ID != user).Select(x => new UserSM(x)).ToList();
+            var conversations = chatContextWCF.UserInConversation.Where(x => x.UserID == user).Select(x => x.Conversation).ToList();
+            result.AddRange(conversations.Select(x => new UserSM(x)));
+            return result;
         }
 
         public List<UserSM> FindFriends(string keys)
@@ -32,12 +51,12 @@ namespace ChatWCF
 
         public List<UserSM> FriendList(int user)
         {
-            throw new NotImplementedException();
+            return new List<UserSM>();
         }
 
         public bool InvitationAnswer(int user, int initiation, bool answer)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
--- ChatFurie/ChatFurie/ChatWCF/Models/SendModels/ConversationUserSM.cs
+++ ChatFurie/ChatFurie/ChatWCF/Models/SendModels/ConversationUserSM.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 using ChatFurie.Models.ChatModel;
 
 namespace ChatWCF.Models.SendModels
 {
+    [DataContract]
     public class ConversationUserSM : UserSM
     {
         public ConversationUserSM(User user) : base(user)
         {
         }
 
+        [DataMember]
         public bool IsAdmin { get; set; }
     }
 }
