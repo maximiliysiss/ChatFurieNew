4f6e3558de914934a7f99184549cfa3a758d716f
Maxim Zimin <zim.ma2010@yandex.ru>
2018-12-21 03:12:13 +0300

    IB-9 CreatClient

---
+++ ChatFurie/ChatFurie/ChatFurie/Middleware/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace ChatFurie.Middleware.Models
+{
+    public class Client
+    {
+        public WebSocket Socket { get; set; }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- ChatFurie/ChatFurie/UnitTestsChatFurie/WCFTest/ChatWCFTest.cs
+++ ChatFurie/ChatFurie/UnitTestsChatFurie/WCFTest/ChatWCFTest.cs
@@ -1,14 +1,65 @@
-﻿using System;
+﻿using AuthServiceWCF.Helpers;
+using AuthServiceWCF.Services;
+using ChatWCF.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace UnitTestsChatFurie.WCFTest
 {
-    class ChatWCFTest : IDisposable
+    public class ChatWCFTest : IDisposable
     {
+        public int u1, u2;
+
+        public ChatWCFTest()
+        {
+            AuthServiceWCF.AuthService authService = new AuthServiceWCF.AuthService();
+            u1 = int.Parse((authService.Register(new AuthServiceWCF.Models.RegisterModel
+            {
+                Birthday = new DateTime(1990, 1, 1),
+                Password = "testingOne",
+                ConfirmedPassword = "testingOne",
+                Email = "test1@mail.ru"
+            }) as RegisterActionResult).Id);
+            u2 = int.Parse((authService.Register(new AuthServiceWCF.Models.RegisterModel
+            {
+                Birthday = new DateTime(1990, 1, 1),
+                Password = "testingOne",
+                ConfirmedPassword = "testingOne",
+                Email = "test2@mail.ru"
+            }) as RegisterActionResult).Id);
+        }
+
+        [Fact]
+        public void AddFriend()
+        {
+            ChatWCF.ChatService chatService = new ChatWCF.ChatService();
+            chatService.AddFriend(u1, u2);
+            var notifications = chatService.GetNotifications(u2).FirstOrDefault();
+            Assert.NotNull(notifications);
+            Assert.Equal(u1, notifications.CreatorID);
+            Assert.Contains("add", notifications.Context.ToLower());
+            chatService.DeclineFriend(u2, notifications.ID);
+        }
+
+        [Fact]
+        public void FindingUsers()
+        {
+            ChatWCF.ChatService chatService = new ChatWCF.ChatService();
+            var user = chatService.FindFriends("test2@mail.ru", u1).FirstOrDefault();
+            Assert.NotNull(user);
+            Assert.Equal(u2, user.ID);
+        }
+
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            AuthServiceWCF.Models.ChatContext chatContext = new AuthServiceWCF.Models.ChatContext();
+            chatContext.Users.Remove(chatContext.Users.Find(u1));
+            chatContext.Users.Remove(chatContext.Users.Find(u2));
+            chatContext.SaveChanges();
         }
     }
 }
